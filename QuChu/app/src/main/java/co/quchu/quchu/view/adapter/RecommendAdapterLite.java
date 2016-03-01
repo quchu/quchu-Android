bf1ac0d8b56ec632ef861ed98051e734b6ac582a
TacticalTwerking <scratchryan@hotmail.com>
2016-03-01 16:38:39 +0800

    分类详情调整

---
+++ QuChu/app/src/main/java/co/quchu/quchu/view/adapter/RecommendAdapterLite.java
@@ -0,0 +1,163 @@
+package co.quchu.quchu.view.adapter;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import co.quchu.quchu.R;
+import co.quchu.quchu.model.RecommendModel;
+import co.quchu.quchu.utils.FlyMeUtils;
+import co.quchu.quchu.utils.StringUtils;
+import co.quchu.quchu.widget.HorizontalNumProgressBar;
+import co.quchu.quchu.widget.ratingbar.ProperRatingBar;
+
+/**
+ * RecommendAdapter
+ * User: Chenhs
+ * Date: 2015-12-08
+ * 趣处推荐 适配器 adapter
+ */
+public class RecommendAdapterLite extends RecyclerView.Adapter<RecommendAdapterLite.RecommendHolder> {
+
+
+    private Activity mContext;
+    private boolean isFlyme = false;
+    private List<RecommendModel> arrayList;
+    private CardClickListener listener;
+
+    public RecommendAdapterLite(Activity mContext, CardClickListener listener) {
+        this.mContext = mContext;
+        isFlyme = FlyMeUtils.isFlyme();
+        this.listener = listener;
+    }
+
+    public void changeDataSet(List<RecommendModel> arrayList) {
+        this.arrayList = arrayList;
+        notifyDataSetChanged();
+    }
+
+    public void loadMoreDataSet(ArrayList<RecommendModel> arrayList) {
+        this.arrayList.addAll(arrayList);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public RecommendAdapterLite.RecommendHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new RecommendHolder(LayoutInflater.from(mContext).inflate(R.layout.item_recommend_cardview_lite, parent, false), listener);
+    }
+
+    @Override
+    public void onBindViewHolder(RecommendAdapterLite.RecommendHolder holder, int position) {
+        RecommendModel model = arrayList.get(position);
+
+        if (!StringUtils.isEmpty(model.getRgb())) {
+            holder.rootCv.setCardBackgroundColor(Color.parseColor("#" + model.getRgb()));
+        } else {
+            holder.rootCv.setCardBackgroundColor(Color.parseColor("#808080"));
+        }
+        holder.itemRecommendCardPhotoSdv.setImageURI(Uri.parse(model.getCover()));
+        if (isFlyme) {
+            holder.itemRecommendCardPhotoSdv.setAspectRatio(1.5f);
+        } else {
+            holder.itemRecommendCardPhotoSdv.setAspectRatio(1.33f);
+        }
+        if (model.isIsActivity()) {
+            holder.item_place_event_tv.setVisibility(View.VISIBLE);
+        } else {
+            holder.item_place_event_tv.setVisibility(View.GONE);
+        }
+        holder.itemRecommendCardAddressTv.setText(model.getAddress());
+        holder.itemRecommendCardCityTv.setText(model.getDescribe());
+        holder.itemRecommendCardNameTv.setText(model.getName());
+        holder.itemRecommendCardPrb.setRating((int) ((model.getSuggest() + 0.5f) >= 5 ? 5 : (model.getSuggest())));
+        holder.itemRecommendCardCollectIv.setImageDrawable(mContext.getResources().
+                getDrawable(model.isIsf() ? R.drawable.ic_detail_collect : R.drawable.ic_detail_uncollect));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (arrayList == null)
+            return 0;
+        else
+            return arrayList.size();
+    }
+
+    class RecommendHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.item_recommend_card_name_tv)
+        TextView itemRecommendCardNameTv;
+        @Bind(R.id.item_recommend_card_city_tv)
+        TextView itemRecommendCardCityTv;
+        @Bind(R.id.item_recommend_card_photo_sdv)
+        SimpleDraweeView itemRecommendCardPhotoSdv;
+        @Bind(R.id.item_recommend_card_prb)
+        ProperRatingBar itemRecommendCardPrb;
+        @Bind(R.id.item_recommend_card_address_tv)
+        TextView itemRecommendCardAddressTv;
+        @Bind(R.id.item_place_event_tv)
+        TextView item_place_event_tv;
+//        @Bind(R.id.item_recommend_card_collect_rl)
+//        RelativeLayout itemRecommendCardCollectRl;
+//        @Bind(R.id.item_recommend_card_interest_rl)
+//        RelativeLayout itemRecommendCardInterestRl;
+//        @Bind(R.id.item_recommend_card_reply_rl)
+//        RelativeLayout itemRecommendCardReplyRl;
+
+        @Bind(R.id.root_cv)
+        CardView rootCv;
+        @Bind(R.id.item_recommend_card_collect_iv)
+        ImageView itemRecommendCardCollectIv;
+        private CardClickListener listener;
+
+        public RecommendHolder(View itemView, CardClickListener listener) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.listener = listener;
+        }
+
+        @OnClick({R.id.root_cv, R.id.item_recommend_card_collect_rl, R.id.item_recommend_card_interest_rl})
+        public void cardClick(View view) {
+            if (isFastDoubleClick())
+                return;
+            if (listener != null)
+                listener.onCardLick(view, getPosition());
+        }
+    }
+
+    private static long lastClickTime = 0L;
+
+    /**
+     * 防止重复点击
+     *
+     * @return
+     */
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        if (time - lastClickTime < 500) {
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+
+    public interface CardClickListener {
+        void onCardLick(View view, int position);
+    }
+
+}
--- QuChu/app/src/main/java/co/quchu/quchu/view/fragment/DefaultRecommendFragment.java
+++ QuChu/app/src/main/java/co/quchu/quchu/view/fragment/DefaultRecommendFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,8 +32,7 @@ import co.quchu.quchu.utils.LogUtils;
 import co.quchu.quchu.utils.SPUtils;
 import co.quchu.quchu.utils.StringUtils;
 import co.quchu.quchu.view.activity.InterestingDetailsActivity;
-import co.quchu.quchu.view.adapter.RecommendAdapter;
-import co.quchu.quchu.widget.recyclerviewpager.RecyclerViewPager;
+import co.quchu.quchu.view.adapter.RecommendAdapterLite;
 
 /**
  * RecommendFragment
@@ -40,11 +40,10 @@ import co.quchu.quchu.widget.recyclerviewpager.RecyclerViewPager;
  * Date: 2015-12-07
  * 推荐 类别点击进入后的
  */
-public class DefaultRecommendFragment extends Fragment implements RecommendAdapter.CardClickListener {
+public class DefaultRecommendFragment extends Fragment implements RecommendAdapterLite.CardClickListener {
     @Bind(R.id.f_recommend_rvp)
-    RecyclerViewPager dfRecommendRvp;
-    @Bind(R.id.f_recommend_bottom_rl)
-    ImageView dfRecommendBottomRl;
+    RecyclerView dfRecommendRvp;
+
     private int dViewHeight = 0;
     public boolean isRunningAnimation = false;
     public ArrayList<RecommendModel> dCardList;
@@ -52,40 +51,39 @@ public class DefaultRecommendFragment extends Fragment implements RecommendAdapt
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recommend_hvp, container,false);
+        View view = inflater.inflate(R.layout.fragment_recommend, container,false);
         ButterKnife.bind(this, view);
-        LinearLayoutManager layout = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager layout = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         dfRecommendRvp.setLayoutManager(layout);
-        adapter = new RecommendAdapter(getActivity(), this);
+        adapter = new RecommendAdapterLite(getActivity(), this);
         dfRecommendRvp.setAdapter(adapter);
         dfRecommendRvp.setHasFixedSize(true);
-        dfRecommendBottomRl.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                dViewHeight = dfRecommendBottomRl.getHeight();
-            }
-        });
-        dfRecommendRvp.addOnScrollListener();
-        dfRecommendRvp.addOnPageChangedListener(new RecyclerViewPager.OnPageChangedListener() {
-            @Override
-            public void OnPageChanged(int oldPosition, int newPosition) {
-                Log.d("test", "oldPosition:" + oldPosition + " newPosition:" + newPosition);
-                if (newPosition <= 2) {
-                    if (dfRecommendBottomRl.getVisibility() == View.VISIBLE)
-                        if (!isRunningAnimation)
-                            RecommendPresenter.showBottomAnimation(DefaultRecommendFragment.this, dfRecommendBottomRl, dViewHeight, false);
-                } else if (newPosition >= 3) {
-                    if (dfRecommendBottomRl.getVisibility() == View.INVISIBLE)
-                        if (!isRunningAnimation)
-                            RecommendPresenter.showBottomAnimation(DefaultRecommendFragment.this, dfRecommendBottomRl, dViewHeight, true);
-                }
-                if (newPosition > oldPosition && dCardList.size() > 3 && newPosition == dCardList.size() - 1 && !isLoading) {
-                    loadMoreDateSet();
-                }
-                indexPosition = newPosition;
-            }
-        });
-        dfRecommendRvp.addOnLayoutChangeListener();
+//        dfRecommendBottomRl.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {c
+//                dViewHeight = dfRecommendBottomRl.getHeight();
+//            }
+//        });
+//        dfRecommendRvp.addOnScrollListener();
+//        dfRecommendRvp.addOnPageChangedListener(new RecyclerViewPager.OnPageChangedListener() {
+//            @Override
+//            public void OnPageChanged(int oldPosition, int newPosition) {
+//                Log.d("test", "oldPosition:" + oldPosition + " newPosition:" + newPosition);
+//                if (newPosition <= 2) {
+//                    if (dfRecommendBottomRl.getVisibility() == View.VISIBLE)
+//                        if (!isRunningAnimation)
+//                            RecommendPresenter.showBottomAnimation(DefaultRecommendFragment.this, dfRecommendBottomRl, dViewHeight, false);
+//                } else if (newPosition >= 3) {
+//                    if (dfRecommendBottomRl.getVisibility() == View.INVISIBLE)
+//                        if (!isRunningAnimation)
+//                            RecommendPresenter.showBottomAnimation(DefaultRecommendFragment.this, dfRecommendBottomRl, dViewHeight, true);
+//                }
+//                if (newPosition > oldPosition && dCardList.size() > 3 && newPosition == dCardList.size() - 1 && !isLoading) {
+//                    loadMoreDateSet();
+//                }
+//                indexPosition = newPosition;
+//            }
+//        });
         if (!StringUtils.isEmpty(SPUtils.getValueFromSPMap(getActivity(), AppKey.USERSELECTEDCLASSIFY, ""))) {
             changeDataSetFromServer();
         }
@@ -95,17 +93,17 @@ public class DefaultRecommendFragment extends Fragment implements RecommendAdapt
     }
 
     private boolean isLoading = false;
-    private RecommendAdapter adapter;
+    private RecommendAdapterLite adapter;
     private int indexPosition = 0;
 
-    @OnClick(R.id.f_recommend_bottom_rl)
-    public void bottomClick(View view) {
-        if (indexPosition > 5) {
-            dfRecommendRvp.scrollToPosition(0);
-        } else {
-            dfRecommendRvp.smoothScrollToPosition(0);
-        }
-    }
+//    @OnClick(R.id.f_recommend_bottom_rl)
+//    public void bottomClick(View view) {
+//        if (indexPosition > 5) {
+//            dfRecommendRvp.scrollToPosition(0);
+//        } else {
+//            dfRecommendRvp.smoothScrollToPosition(0);
+//        }
+//    }
 
     @Override
     public void onDestroyView() {
@@ -123,8 +121,8 @@ public class DefaultRecommendFragment extends Fragment implements RecommendAdapt
                 dCardList = arrayList;
                 adapter.changeDataSet(dCardList);
                 dfRecommendRvp.smoothScrollToPosition(0);
-                if (dfRecommendBottomRl.getVisibility() == View.VISIBLE)
-                    RecommendPresenter.showBottomAnimation(DefaultRecommendFragment.this, dfRecommendBottomRl, dViewHeight, false);
+//                if (dfRecommendBottomRl.getVisibility() == View.VISIBLE)
+//                    RecommendPresenter.showBottomAnimation(DefaultRecommendFragment.this, dfRecommendBottomRl, dViewHeight, false);
             }
 
             @Override
