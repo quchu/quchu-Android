4bbcb8200a4836cd81bdffaf503b4dcd4f35b0a2
TacticalTwerking <scratchryan@hotmail.com>
2016-04-18 11:34:44 +0800

    发现详情布局调整

--- QuChu/app/src/main/java/co/quchu/quchu/view/activity/ClassifyDetailActivity.java
+++ QuChu/app/src/main/java/co/quchu/quchu/view/activity/ClassifyDetailActivity.java
@@ -2,36 +2,26 @@ package co.quchu.quchu.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import co.quchu.quchu.R;
-import co.quchu.quchu.analysis.GatherCollectModel;
-import co.quchu.quchu.base.AppContext;
 import co.quchu.quchu.base.BaseActivity;
-import co.quchu.quchu.dialog.ShareDialogFg;
-import co.quchu.quchu.dialog.VisitorLoginDialogFg;
 import co.quchu.quchu.model.RecommendModel;
-import co.quchu.quchu.presenter.InterestingDetailPresenter;
 import co.quchu.quchu.presenter.RecommendPresenter;
-import co.quchu.quchu.view.adapter.ClassifyDecoration;
+import co.quchu.quchu.view.adapter.DiscoverDetailPagerAdapter;
 import co.quchu.quchu.view.adapter.RecommendAdapterLite;
 
-public class ClassifyDetailActivity extends BaseActivity implements RecommendAdapterLite.CardClickListener {
+public class ClassifyDetailActivity extends BaseActivity {
 
-
-    @Bind(R.id.title_content_tv)
-    TextView titleContentTv;
-    @Bind(R.id.recyclerView)
-    RecyclerView recyclerView;
-    public ArrayList<RecommendModel> dCardList;
+    @Bind(R.id.vpContent)
+    ViewPager vpContent;
+    public ArrayList<RecommendModel> mData = new ArrayList<>();
+    public DiscoverDetailPagerAdapter mAdapter;
 
     public static final String PARAMETER_TITLE = "title";
 
@@ -40,15 +30,27 @@ public class ClassifyDetailActivity extends BaseActivity implements RecommendAda
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classify_detail);
         ButterKnife.bind(this);
-        initTitleBar();
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new ClassifyDecoration(this));
-        adapter = new RecommendAdapterLite(this, this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setHasFixedSize(true);
-        titleContentTv.setText(getIntent().getExtras().getString(PARAMETER_TITLE));
-        changeDataSetFromServer();
+        getEnhancedToolbar();
+
+        mAdapter = new DiscoverDetailPagerAdapter(mData, this, new DiscoverDetailPagerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = new Intent(ClassifyDetailActivity.this, QuchuDetailsActivity.class);
+                intent.putExtra(QuchuDetailsActivity.REQUEST_KEY_PID, mData.get(position).getPid());
+                startActivity(intent);
+            }
+        });
+        vpContent.setAdapter(mAdapter);
+        vpContent.setClipToPadding(false);
+        vpContent.setPadding(80,40,80,40);
+        vpContent.setPageMargin(40);
+        vpContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        getDataSetFromServer();
     }
 
     @Override
@@ -56,16 +58,15 @@ public class ClassifyDetailActivity extends BaseActivity implements RecommendAda
         return TRANSITION_TYPE_LEFT;
     }
 
-    private RecommendAdapterLite adapter;
 
 
-    public void changeDataSetFromServer() {
+    public void getDataSetFromServer() {
         RecommendPresenter.getRecommendList(this, false, new RecommendPresenter.GetRecommendListener() {
             @Override
             public void onSuccess(ArrayList<RecommendModel> arrayList, int pageCount, int pageNum) {
-                dCardList = arrayList;
-                adapter.changeDataSet(dCardList);
-                recyclerView.smoothScrollToPosition(0);
+                mData.clear();
+                mData.addAll(arrayList);
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -78,49 +79,5 @@ public class ClassifyDetailActivity extends BaseActivity implements RecommendAda
     }
 
 
-    @Override
-    public void onCardLick(View view, int position) {
 
-        switch (view.getId()) {
-            case R.id.root_cv:
-                AppContext.selectedPlace = dCardList.get(position);
-                Intent intent = new Intent(this, QuchuDetailsActivity.class);
-                intent.putExtra(QuchuDetailsActivity.REQUEST_KEY_PID, dCardList.get(position).getPid());
-                this.startActivity(intent);
-                break;
-            case R.id.item_recommend_card_collect_rl:
-                setFavorite(position);
-                break;
-            case R.id.item_recommend_card_interest_rl:
-                ShareDialogFg shareDialogFg = ShareDialogFg.newInstance(dCardList.get(position).getPid(), dCardList.get(position).getName(), true);
-                shareDialogFg.show(this.getFragmentManager(), "share_place");
-                break;
-        }
-    }
-
-    private void setFavorite(final int position) {
-        if (AppContext.user.isIsVisitors()) {
-            VisitorLoginDialogFg vDialog = VisitorLoginDialogFg.newInstance(VisitorLoginDialogFg.QFAVORITE);
-            vDialog.show(this.getFragmentManager(), "visitor");
-        } else {
-            InterestingDetailPresenter.setDetailFavorite(this, dCardList.get(position).getPid(), dCardList.get(position).isIsf(), new InterestingDetailPresenter.DetailDataListener() {
-                @Override
-                public void onSuccessCall(String str) {
-                    dCardList.get(position).setIsf(!dCardList.get(position).isIsf());
-                    adapter.notifyDataSetChanged();
-                    if (dCardList.get(position).isIsf()) {
-                        Toast.makeText(ClassifyDetailActivity.this, "收藏成功!", Toast.LENGTH_SHORT).show();
-                        AppContext.gatherList.add(new GatherCollectModel(GatherCollectModel.collectPlace, dCardList.get(position).getPid()));
-                    } else {
-                        Toast.makeText(ClassifyDetailActivity.this, "取消收藏!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                @Override
-                public void onErrorCall(String str) {
-
-                }
-            });
-        }
-    }
 }
--- QuChu/app/src/main/java/co/quchu/quchu/view/adapter/DiscoverDetailPagerAdapter.java
+++ QuChu/app/src/main/java/co/quchu/quchu/view/adapter/DiscoverDetailPagerAdapter.java
@@ -1,9 +1,10 @@
 package co.quchu.quchu.view.adapter;
 
-import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.Spannable;
@@ -12,7 +13,6 @@ import android.text.style.DynamicDrawableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -22,48 +22,43 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import co.quchu.galleryfinal.utils.ImageUtils;
 import co.quchu.quchu.R;
 import co.quchu.quchu.model.RecommendModel;
-import co.quchu.quchu.utils.FlyMeUtils;
 import co.quchu.quchu.utils.SPUtils;
 import co.quchu.quchu.utils.StringUtils;
-import co.quchu.quchu.widget.TagCloudView;
 import co.quchu.quchu.widget.ratingbar.ProperRatingBar;
 
 /**
- * RecommendAdapter
- * User: Chenhs
- * Date: 2015-12-08
- * 趣处推荐 适配器 adapter
+ * Created by Nico on 16/4/7.
  */
-public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.RecommendHolder> {
+public class DiscoverDetailPagerAdapter extends PagerAdapter {
 
+    private Context mContext;
+    private List<RecommendModel> mData;
+    private OnItemClickListener mListener;
 
-    private Activity mContext;
-    private boolean isFlyme = false;
-    private List<RecommendModel> dataSet;
-    private CardClickListener listener;
-
-    public RecommendAdapter(Activity mContext, List<RecommendModel> arrayList, CardClickListener listener) {
-        this.mContext = mContext;
-        isFlyme = FlyMeUtils.isFlyme();
-        dataSet = arrayList;
-        this.listener = listener;
-    }
-
-
-    @Override
-    public RecommendAdapter.RecommendHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new RecommendHolder(LayoutInflater.from(mContext).inflate(R.layout.item_recommend_cardview_new_miui, parent, false), listener);
+    public DiscoverDetailPagerAdapter(List<RecommendModel> pData, Context context,OnItemClickListener pListener) {
+        this.mData = pData;
+        this.mContext = context;
+        this.mListener = pListener;
     }
 
     @Override
-    public void onBindViewHolder(RecommendAdapter.RecommendHolder holder, int position) {
-        RecommendModel model = dataSet.get(position);
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    @Override
+    public View instantiateItem(ViewGroup container, final int position) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_recommend_cardview_new_miui, container, false);
+        container.addView(view);
+        ViewHolder holder = new ViewHolder(view);
+        RecommendModel model = mData.get(position);
         holder.rootCv.setCardBackgroundColor(Color.parseColor("#E6EEEFEF"));
-        //ImageUtils.loadWithAppropriateSize(holder.itemRecommendCardPhotoSdv,Uri.parse(model.getCover()));
         holder.itemRecommendCardPhotoSdv.setImageURI(Uri.parse(model.getCover()));
         if (model.isIsActivity()) {
             holder.item_place_event_tv.setVisibility(View.VISIBLE);
@@ -73,16 +68,7 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
         holder.itemRecommendCardAddressTv.setText(model.getAddress());
         holder.itemRecommendCardPrb.setRating((int) ((model.getSuggest() + 0.5f) >= 5 ? 5 : (model.getSuggest())));
         holder.item_recommend_card_name_tv.setText(model.getName());
-
-//        List<RecommendModel.GenesEntity> genes = model.getGenes();
-//        holder.tag1.setText(genes.get(0).getKey());
-//        holder.tag2.setText(genes.get(1).getKey());
-//        holder.tag3.setText(genes.get(2).getKey());
-
-        //holder.itemRecommendCardCollectIv.setImageDrawable(mContext.getResources().getDrawable(model.isIsf() ? R.mipmap.ic_detail_collect_dark : R.mipmap.ic_atmophere_unselected_dark));
-
-        if (model.isout) {//用户去过该趣处
-            //去过标签 start
+        if (model.isout) {
             SpannableString spanText = new SpannableString(model.getName() + "，");
             DynamicDrawableSpan drawableSpan2 = new DynamicDrawableSpan(
                     DynamicDrawableSpan.ALIGN_BOTTOM) {
@@ -96,12 +82,10 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
             spanText.setSpan(drawableSpan2, model.getName().length(), model.getName().length() + 1
                     , Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
             holder.item_recommend_card_name_tv.setText(spanText);
-            //去过标签 end
         } else {
             holder.item_recommend_card_name_tv.setText(model.getName());
         }
         if (null != model.getTags() && model.getTags().size() > 0) {
-            //ArrayList<String> tags = new ArrayList<String>();
             for (int i = 0; i < model.getTags().size(); i++) {
                 switch (i){
                     case 0:
@@ -118,10 +102,7 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
                         break;
                 }
             }
-//            holder.detailStoreTagcloundTcv.setVisibility(View.VISIBLE);
-//            holder.detailStoreTagcloundTcv.setTags(tags);
         } else {
-            //holder.detailStoreTagcloundTcv.setVisibility(View.INVISIBLE);
             holder.tag1.setVisibility(View.GONE);
             holder.tag2.setVisibility(View.GONE);
             holder.tag3.setVisibility(View.GONE);
@@ -131,24 +112,19 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
         } else {
             String distance = StringUtils.formatDouble(Double.parseDouble(model.getDistance())) + "km";
             holder.item_recommend_card_distance_tv.setText("距您" + distance);
-//            if (StringUtils.isDouble(model.getDistance())) {
-//
-//                holder.item_recommend_card_distance_tv.setText("距您" + distance);
-//                StringUtils.alterBoldTextColor(holder.item_recommend_card_distance_tv, 2, 2 + distance.length(), R.color.white);
-//            }
         }
-
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null!=mListener){
+                    mListener.onItemClick(position);
+                }
+            }
+        });
+        return view;
     }
 
-    @Override
-    public int getItemCount() {
-        if (dataSet == null)
-            return 0;
-        else
-            return dataSet.size();
-    }
-
-    class RecommendHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder {
         @Bind(R.id.item_recommend_card_photo_sdv)
         SimpleDraweeView itemRecommendCardPhotoSdv;
         @Bind(R.id.item_recommend_card_prb)
@@ -159,62 +135,34 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
         TextView item_place_event_tv;
         @Bind(R.id.item_recommend_card_name_tv)
         TextView item_recommend_card_name_tv;
-
         @Bind(R.id.recommend_tag1)
         TextView tag1;
         @Bind(R.id.recommend_tag2)
         TextView tag2;
         @Bind(R.id.recommend_tag3)
         TextView tag3;
-
         @Bind(R.id.root_cv)
         CardView rootCv;
         @Bind(R.id.item_recommend_card_collect_iv)
         TextView itemRecommendCardCollectIv;
-        //        @Bind(R.id.detail_store_tagclound_tcv)
-//        TagCloudView detailStoreTagcloundTcv;
         @Bind(R.id.item_recommend_card_distance_tv)
         TextView item_recommend_card_distance_tv;
-        private CardClickListener listener;
 
-        public RecommendHolder(View itemView, CardClickListener listener) {
-            super(itemView);
+        public ViewHolder(View itemView) {
             ButterKnife.bind(this, itemView);
-            this.listener = listener;
-//            if (isFlyme) {
-//                itemRecommendCardPhotoSdv.setAspectRatio(1.4f);
-//            } else {
-//                itemRecommendCardPhotoSdv.setAspectRatio(1.2f);
-//            }
-        }
-
-        @OnClick({R.id.root_cv, R.id.item_recommend_card_collect_iv, R.id.item_recommend_card_interest_iv})
-        public void cardClick(View view) {
-            if (isFastDoubleClick())
-                return;
-            if (listener != null)
-                listener.onCardLick(view, getPosition());
         }
     }
 
-    private static long lastClickTime = 0L;
 
-    /**
-     * 防止重复点击
-     *
-     * @return
-     */
-    public static boolean isFastDoubleClick() {
-        long time = System.currentTimeMillis();
-        if (time - lastClickTime < 500) {
-            return true;
-        }
-        lastClickTime = time;
-        return false;
+    @Override
+    public int getCount() {
+
+        return null != mData ? mData.size() : 0;
     }
 
-    public interface CardClickListener {
-        void onCardLick(View view, int position);
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
     }
 
 }
