f94279078c1fb083d62fca125e725d8f324119fb
TacticalTwerking <scratchryan@hotmail.com>
2016-10-26 10:52:53 +0800

    Daily commit

--- QuChu/app/src/main/java/co/quchu/quchu/view/activity/QuchuDetailsActivity.java
+++ QuChu/app/src/main/java/co/quchu/quchu/view/activity/QuchuDetailsActivity.java
@@ -1,17 +1,23 @@
 package co.quchu.quchu.view.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.util.ArrayMap;
+import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import co.quchu.quchu.model.ImageModel;
+import co.quchu.quchu.utils.ScreenUtils;
 import com.android.volley.VolleyError;
 
+import io.rong.imlib.common.DeviceUtils;
 import java.util.ArrayList;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -78,6 +84,34 @@ public class QuchuDetailsActivity extends BaseBehaviorActivity {
     ButterKnife.bind(this);
     from = getIntent().getStringExtra(REQUEST_KEY_FROM);
     getEnhancedToolbar().getTitleTv().setText("");
+
+    TabLayout tabLayout = new TabLayout((new ContextThemeWrapper(this, R.style.AppTheme)));
+    tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.standard_color_h1_dark));
+    tabLayout.setTabMode(TabLayout.MODE_FIXED);
+    tabLayout.setTabTextColors(getResources().getColor(R.color.standard_color_h2_dark),getResources().getColor(R.color.standard_color_h1_dark));
+
+    int tabLayoutWidth = ScreenUtils.getScreenWidth(getApplicationContext());
+    RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(tabLayoutWidth/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+    lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+    getEnhancedToolbar().addCustomView(tabLayout,lp);
+
+    tabLayout.addTab(tabLayout.newTab().setText("趣处").setTag(0));
+    tabLayout.addTab(tabLayout.newTab().setText("文章").setTag(1));
+
+    tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+      @Override public void onTabSelected(TabLayout.Tab tab) {
+        if((int)tab.getTag()==0){
+          findViewById(R.id.flContainer).setVisibility(View.INVISIBLE);
+        }else{
+          findViewById(R.id.flContainer).setVisibility(View.VISIBLE);
+        }
+      }
+
+      @Override public void onTabUnselected(TabLayout.Tab tab) {}
+
+      @Override public void onTabReselected(TabLayout.Tab tab) {}
+    });
+
 
     if (null != savedInstanceState) {
       dModel = (DetailModel) savedInstanceState.getSerializable(BUNDLE_KEY_DATA_MODEL);
@@ -179,6 +213,7 @@ public class QuchuDetailsActivity extends BaseBehaviorActivity {
       bundle.putInt(CommentListFragment.BUNDLE_KEY_PLACE_ID,pId);
       mCommentListFragment.setArguments(bundle);
       getSupportFragmentManager().beginTransaction().add(R.id.flContainer,mCommentListFragment).commitAllowingStateLoss();
+      findViewById(R.id.flContainer).setVisibility(View.INVISIBLE);
 
     }
   }
