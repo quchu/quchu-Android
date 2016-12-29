092094c1e4c5f75a89898a3c6c9a589ee5e6f248
TacticalTwerking <scratchryan@hotmail.com>
2016-12-29 17:39:30 +0800

    Daily commit

--- QuChu/app/src/main/java/co/quchu/quchu/widget/XiaoQFab.java
+++ QuChu/app/src/main/java/co/quchu/quchu/widget/XiaoQFab.java
@@ -28,17 +28,24 @@ import co.quchu.quchu.R;
 public class XiaoQFab extends FloatingActionButton {
 
   private boolean mAnimationRunning;
-  private float[] mAnimationProgress;
-  private int mSides = 5;
   public boolean mLoading = false;
-  private int mSize = -1;
+  private float mAnimationProgress;
   private float mStrokeWidth = -1;
+  private int mSize = -1;
   private int mHalfSize = -1;
   private int mOffSetOutBlk = -1;
   private int mOffSetSec = -1;
   private int mOffSetThd = -1;
   private int mOffSetF = -1;
   private boolean mShowPromote = false;
+  private boolean mRevert = false;
+  private boolean mLoadingAnimationStart = false;
+  private float mLoadingDotsFact = 0;
+
+  private float[] mAnimationValueSet = new float[]{1,1,1};
+
+  private float mLoadingDotsRadius = -1;
+
   private Bitmap mBitmapAlice;
 
   public XiaoQFab(Context context) {
@@ -53,195 +60,172 @@ public class XiaoQFab extends FloatingActionButton {
     super(context, attrs, defStyleAttr);
   }
 
-
-
   @Override protected void onDraw(Canvas canvas) {
 
-    if(-1==mSize)mSize =  Math.min(getWidth(), getHeight());
-    if(-1==mStrokeWidth) mStrokeWidth = mSize/13f;
-    if(-1==mHalfSize)mHalfSize =  mSize/2;
-    if(-1==mOffSetOutBlk)mOffSetOutBlk =  (mSize / 21);
-    if(-1==mOffSetSec) mOffSetSec = (int) (mSize / 7f);
-    if(-1==mOffSetThd) mOffSetThd = (int) (mSize / 4.5f);
-    if(-1==mOffSetF) mOffSetF = (int) (mSize / 3.15f);
-    if (null==mBitmapAlice){
+    if (-1 == mSize) mSize = Math.min(getWidth(), getHeight());
+    if (-1 == mStrokeWidth) mStrokeWidth = mSize / 13f;
+    if (-1 == mHalfSize) mHalfSize = mSize / 2;
+    if (-1 == mOffSetOutBlk) mOffSetOutBlk = (mSize / 21);
+    if (-1 == mOffSetSec) mOffSetSec = (int) (mSize / 7f);
+    if (-1 == mOffSetThd) mOffSetThd = (int) (mSize / 4.5f);
+    if (-1 == mOffSetF) mOffSetF = (int) (mSize / 3.15f);
+    if (-1 == mLoadingDotsRadius) mLoadingDotsRadius = mSize/15;
+    if (null == mBitmapAlice) {
       mBitmapAlice = BitmapFactory.decodeResource(getResources(), R.drawable.ic_alice);
     }
 
     Paint paint = new Paint();
     paint.setColor(Color.parseColor("#2d313c"));
     paint.setStrokeWidth(mStrokeWidth);
-    paint.setStyle(Paint.Style.STROKE);
+    paint.setStyle(Paint.Style.FILL);
     paint.setAntiAlias(true);
     paint.setStrokeCap(Paint.Cap.ROUND);
-
 
     Paint bg = new Paint();
     bg.setColor(Color.WHITE);
     bg.setStyle(Paint.Style.FILL);
     bg.setAntiAlias(true);
 
+    canvas.drawCircle(mHalfSize, mHalfSize, mHalfSize - (mStrokeWidth / 2), bg);
 
-    canvas.drawCircle(mHalfSize,mHalfSize,mHalfSize-(mStrokeWidth/2),bg);
+    //canvas.drawBitmap(mBitmapAlice, (getWidth() - mBitmapAlice.getWidth()) / 2,
+    //    (getHeight() - mBitmapAlice.getHeight()) / 2 - mOffSetOutBlk, paint);
 
-
-
-    if (!mLoadingAnimationStart){
-      if (null != mAnimationProgress) {
-
-        float radius1 = 360 *mAnimationProgress[0];
-        float radius2 = 360 *mAnimationProgress[1];
-        float radius3 = 360 *mAnimationProgress[2];
-        float radius4 = 360 *mAnimationProgress[3];
-
-        canvas.drawArc(new RectF(mOffSetOutBlk, mOffSetOutBlk, mSize - mOffSetOutBlk, mSize - mOffSetOutBlk),
-            radius1, radius1, false, paint);
-
-        paint.setColor(Color.WHITE);
+    Paint pCircle = new Paint();
+    pCircle.setStyle(Paint.Style.FILL);
+    pCircle.setColor(Color.WHITE);
 
 
-//        canvas.drawArc(new RectF(mOffSetSec, mOffSetSec, mSize - mOffSetSec, mSize - mOffSetSec),
-//            180 * mAnimationProgress[1], radius2, false, paint);
-//
-//        paint.setStrokeWidth(mSize/12);
-//        paint.setColor(Color.parseColor("#ffd702"));
-//
-//        canvas.drawArc(new RectF(mOffSetThd, mOffSetThd, mSize - mOffSetThd, mSize - mOffSetThd),
-//            120 * mAnimationProgress[2], radius3, false, paint);
-//
-//        paint.setStrokeWidth(mSize/9);
-//        paint.setColor(Color.BLACK);
-//        canvas.drawArc(new RectF(mOffSetF, mOffSetF, mSize - mOffSetF, mSize - mOffSetF),
-//            240 * mAnimationProgress[3], radius4, false, paint);
-//
-//        canvas.save();
-//        canvas.rotate(-180 + (mAnimationProgress[4] * 180), getWidth() / 2, getHeight() / 2);
-//        paint.setStyle(Paint.Style.FILL);
-//        paint.setColor(Color.parseColor("#eaeaea"));
-//        canvas.drawCircle(mSize / 4, mSize - (mSize/4), (mHalfSize/7) * mAnimationProgress[3], paint);
-//        canvas.restore();
 
-      }
+    float centerWidth = getWidth()/2;
+    float centerHeigh = getHeight()/2 - mOffSetOutBlk;
 
+    Paint p1,p2,p3;
+    p1 = new Paint(paint);
+    p1.setColor(Color.parseColor("#F44336"));
 
+    p2 = new Paint(p1);
+    p2.setColor(Color.parseColor("#4CAF50"));
+
+    p3 = new Paint(p2);
+    p3.setColor(Color.parseColor("#03A9F4"));
+    canvas.drawBitmap(mBitmapAlice, (getWidth() - mBitmapAlice.getWidth()) / 2,
+        (getHeight() - mBitmapAlice.getHeight()) / 2 - mOffSetOutBlk, paint);
+
+    if (mLoadingAnimationStart){
+      canvas.drawArc(
+          new RectF(mOffSetOutBlk, mOffSetOutBlk, mSize - mOffSetOutBlk, mSize - mOffSetOutBlk), 20,
+          360* mAnimationProgress, true, pCircle);
+
+      canvas.drawCircle((centerWidth/2)+(1-mLoadingDotsFact)*(centerWidth/2),centerHeigh,mLoadingDotsRadius*mAnimationValueSet[0]*mLoadingDotsFact,p2);
+      canvas.drawCircle(centerWidth,centerHeigh,mLoadingDotsRadius*mAnimationValueSet[1]*mLoadingDotsFact,p1);
+      canvas.drawCircle(centerWidth+((centerWidth/2)*mLoadingDotsFact),centerHeigh,mLoadingDotsRadius*mAnimationValueSet[2]*mLoadingDotsFact,p3);
     }else{
-      //canvas.save();
-      //canvas.rotate((revert?360:-360)* mAnimationProgress[0],getWidth()/2,getHeight()/2);
-      canvas.drawArc(new RectF(mOffSetOutBlk, mOffSetOutBlk, mSize - mOffSetOutBlk, mSize - mOffSetOutBlk),
-          (revert?360:-360)  * mAnimationProgress[0], (revert?360:-360)  * mAnimationProgress[0], false, paint);
-      //canvas.restore();
+
+      canvas.drawArc(
+          new RectF(mOffSetOutBlk, mOffSetOutBlk, mSize - mOffSetOutBlk, mSize - mOffSetOutBlk), 20,
+          (mRevert ? 360 : -360) * mAnimationProgress, true, pCircle);
     }
-
-
-    if (mShowPromote){
-      float width = getWidth()/2.5f;
-      float indicatorDiameter = width/2.5f;
-
-      float whiteDotsSize = indicatorDiameter/5;
-      float radius = indicatorDiameter/2;
-
-      Paint paintIndicator = new Paint();
-      paintIndicator.setColor(Color.RED);
-      paintIndicator.setStyle(Paint.Style.FILL);
-      paintIndicator.setAntiAlias(true);
-      float offset = getWidth()-width;
-      float verticalOffset = 10;
-      canvas.drawRect(offset+radius,indicatorDiameter+verticalOffset,getWidth()-radius,indicatorDiameter+indicatorDiameter+verticalOffset,paintIndicator);
-      canvas.drawCircle(offset+radius,radius+indicatorDiameter+verticalOffset,radius,paintIndicator);
-      canvas.drawCircle(getWidth()-radius,radius+indicatorDiameter+verticalOffset,radius,paintIndicator);
-
-      Paint paintIndicatorWhite = new Paint(paintIndicator);
-      paintIndicatorWhite.setColor(Color.WHITE);
-      canvas.drawCircle(getWidth()-radius,radius+indicatorDiameter+verticalOffset,whiteDotsSize,paintIndicatorWhite);
-      canvas.drawCircle(offset+radius,radius+indicatorDiameter+verticalOffset,whiteDotsSize,paintIndicatorWhite);
-      canvas.drawCircle(offset+(width/2),radius+indicatorDiameter+verticalOffset,whiteDotsSize,paintIndicatorWhite);
-    }
-
-    canvas.drawBitmap(mBitmapAlice,(getWidth()-mBitmapAlice.getWidth())/2 ,(getHeight()-mBitmapAlice.getHeight())/2 -mOffSetOutBlk,paint);
 
   }
 
-
-  public void setPromote(boolean showPromote){
+  public void setPromote(boolean showPromote) {
     mShowPromote = showPromote;
     invalidate();
   }
 
-  public void endLoading(){
-    if (!mLoading){
+  public void endLoading() {
+    if (!mLoading) {
       return;
     }
     mLoading = false;
   }
 
-
   public void animateLoading() {
-    if (mAnimationRunning||mLoadingAnimationStart) {
+    if (mAnimationRunning || mLoadingAnimationStart) {
       return;
     }
     mLoading = true;
 
-    ValueAnimator animator = new ValueAnimator().ofFloat(1,0);
-    animator.setDuration(750);
-    animator.setInterpolator(new AccelerateDecelerateInterpolator());
-    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+    ValueAnimator animationHideAlice = new ValueAnimator().ofFloat(0, 1);
+    animationHideAlice.setDuration(750);
+    animationHideAlice.setInterpolator(new AccelerateDecelerateInterpolator());
+    animationHideAlice.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
       @Override public void onAnimationUpdate(ValueAnimator animation) {
-
         float i = (float) animation.getAnimatedValue();
-        for (int j = 0; j < mSides; j++) {
-          mAnimationProgress[j]=i;
-        }
+        mAnimationProgress = i;
         invalidate();
       }
     });
-    animator.start();
 
-    ValueAnimator animator2 = new ValueAnimator().ofFloat(0,1);
-    animator2.setDuration(2000);
-    animator2.setInterpolator(new LinearInterpolator());
-    animator2.setStartDelay(500);
-    animator2.setRepeatMode(ValueAnimator.REVERSE);
-    animator2.setRepeatCount(ValueAnimator.INFINITE);
-    animator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+    animationHideAlice.start();
+
+    ValueAnimator dotsAppearAnimation = new ValueAnimator().ofFloat(0,1);
+    dotsAppearAnimation.setStartDelay(750);
+    dotsAppearAnimation.setDuration(500);
+    dotsAppearAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
       @Override public void onAnimationUpdate(ValueAnimator animation) {
-        float i = (float) animation.getAnimatedValue();
-        mAnimationProgress[0] = i;
+        mLoadingDotsFact = (float) animation.getAnimatedValue();
         invalidate();
       }
     });
-    animator2.addListener(new Animator.AnimatorListener() {
+    dotsAppearAnimation.addListener(new Animator.AnimatorListener() {
+
       @Override public void onAnimationStart(Animator animation) {
         mLoadingAnimationStart = true;
       }
 
-      @Override public void onAnimationEnd(Animator animation) {
-        mLoadingAnimationStart = false;
-        revert = false;
-        animateInitial();
-      }
+      @Override public void onAnimationEnd(Animator animation) {}
 
-      @Override public void onAnimationCancel(Animator animation) {
-        mLoadingAnimationStart = false;
-      }
+      @Override public void onAnimationCancel(Animator animation) {}
 
-      @Override public void onAnimationRepeat(Animator animation) {
-        revert = !revert;
-        if (!mLoading){
-          animation.end();
-        }
-      }
+      @Override public void onAnimationRepeat(Animator animation) {}
     });
-    animator2.start();
+    dotsAppearAnimation.start();
+
+
+    for (int i = 0; i < 3; i++) {
+      ValueAnimator valueAnimator = new ValueAnimator().ofFloat(1,.5f);
+      valueAnimator.setDuration(500);
+      valueAnimator.setInterpolator(new LinearInterpolator());
+      valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+      valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+      final int finalI = i;
+      valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        @Override public void onAnimationUpdate(ValueAnimator animation) {
+          mAnimationValueSet[finalI] = (float) animation.getAnimatedValue();
+          invalidate();
+        }
+      });
+      valueAnimator.setStartDelay((i*200)+750+500);
+      valueAnimator.start();
+      valueAnimator.addListener(new Animator.AnimatorListener() {
+        @Override public void onAnimationStart(Animator animation) {}
+
+        @Override public void onAnimationEnd(Animator animation) {
+          mLoadingAnimationStart = false;
+          mRevert = false;
+          animateInitial();
+        }
+
+        @Override public void onAnimationCancel(Animator animation) {
+          mLoadingAnimationStart = false;
+        }
+
+        @Override public void onAnimationRepeat(Animator animation) {
+          mRevert = !mRevert;
+          if (!mLoading && mRevert) {
+            animation.end();
+          }
+        }
+      });
+    }
+
 
   }
 
-  private boolean revert = false;
-
-  private boolean mLoadingAnimationStart = false;
-
-
-  public void animateInitial(){
-    mAnimationProgress = new float[mSides];
+  public void animateInitial() {
+    mAnimationProgress = 1;
     animateInitial(-1);
   }
 
@@ -251,60 +235,37 @@ public class XiaoQFab extends FloatingActionButton {
       return;
     }
 
-    for (int i = 0; i < mSides; i++) {
-      mAnimationRunning = true;
-      final int finalI = i;
-      ValueAnimator animator;
+    mAnimationRunning = true;
+    ValueAnimator animator;
 
-      if (startAt == -1){
-        animator = ValueAnimator.ofFloat(0, 1);
-      }else {
-        animator = ValueAnimator.ofFloat(startAt,0,1);
-      }
-
-
-      animator.setDuration(500);
-      animator.setInterpolator(new AccelerateDecelerateInterpolator());
-      animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-        @Override public void onAnimationUpdate(ValueAnimator animation) {
-          if (finalI==4){
-            return;
-          }
-          mAnimationProgress[finalI] = (float) animation.getAnimatedValue();
-          invalidate();
-        }
-      });
-      animator.setStartDelay(100 * i);
-      if (finalI == mSides - 1) {
-        animator.addListener(new Animator.AnimatorListener() {
-          @Override public void onAnimationStart(Animator animation) {}
-
-          @Override public void onAnimationEnd(Animator animation) {
-            mAnimationRunning = false;
-          }
-
-          @Override public void onAnimationCancel(Animator animation) {
-            mAnimationRunning = false;
-          }
-
-          @Override public void onAnimationRepeat(Animator animation) {}
-        });
-      }
-      animator.start();
-
+    if (startAt == -1) {
+      animator = ValueAnimator.ofFloat(1, 0);
+    } else {
+      animator = ValueAnimator.ofFloat(startAt, 0);
     }
 
-    ValueAnimator animator1 = new ValueAnimator().ofFloat(0, 1);
-    animator1.setDuration(700);
-    animator1.setStartDelay(500);
-    animator1.setInterpolator(new OvershootInterpolator(4f));
-    animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+    animator.setDuration(750);
+    animator.setInterpolator(new AccelerateDecelerateInterpolator());
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
       @Override public void onAnimationUpdate(ValueAnimator animation) {
-        mAnimationProgress[4] = (float) animation.getAnimatedValue();
+        mAnimationProgress = (float) animation.getAnimatedValue();
         invalidate();
       }
     });
-    animator1.start();
-  }
+    animator.addListener(new Animator.AnimatorListener() {
+      @Override public void onAnimationStart(Animator animation) {}
 
+      @Override public void onAnimationEnd(Animator animation) {
+        mAnimationRunning = false;
+      }
+
+      @Override public void onAnimationCancel(Animator animation) {
+        mAnimationRunning = false;
+      }
+
+      @Override public void onAnimationRepeat(Animator animation) {}
+
+    });
+    animator.start();
+  }
 }
